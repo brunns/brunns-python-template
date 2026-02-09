2bf9e02151abda0a4eedc2d08f0c4d79a3f020e1
Simon Brunning <simon@brunningonline.net>
2026-02-09 11:23:33 +0000

    Use gum for package selection.

--- README.md
+++ README.md
@@ -2,24 +2,24 @@
 
 ## Dependencies
 
-Requires [uv](https://docs.astral.sh/uv/) and [xc](https://xcfile.dev/).
+Requires [uv](https://docs.astral.sh/uv/), [gum](https://github.com/charmbracelet/gum), [colima](https://github.com/abiosoft/colima) and [xc](https://xcfile.dev/).
 
 Uses `uv` to set up the project, [https://www.toptal.com/developers/gitignore](https://www.toptal.com/developers/gitignore) 
 to add a `.gitignore`, then adds additional files from the `template` directory using [copier](https://github.com/copier-org/copier).
 
 ```sh
-brew install uv xc && brew upgrade uv xc
+brew install uv gum colima xc && brew upgrade uv gum colima xc
 ```
 
 ## Instructions
 
-The `build.sh` script takes 3 arguments:
+The `build.sh` script takes 2 arguments:
 
 * Package name
 * Package description
-* Template location
-    * (This argument requires an absolute path. Tip - use `realpath` to get one.)
 
 ```sh
-./build.sh "sausages" "Sausages thing" `realpath template`
+./build.sh "sausages" "Sausages thing" 
 ```
+
+The user will be prompted if these are not provided.
--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-PACKAGE="${1:-package_name}"
-DESCRIPTION="${2:-Package description.}"
-TEMPLATE_REPO="${3:-template}"
+PACKAGE=$1
+if [ -z "$PACKAGE" ]; then
+    PACKAGE=$(gum input --header "Enter package name" --placeholder "my-awesome-app")
+fi
+
+DESCRIPTION=$2
+if [ -z "$DESCRIPTION" ]; then
+    DESCRIPTION=$(gum input --header "Enter package description" --value "A new project.")
+fi
+
+if [ -z "$PACKAGE" ]; then
+    echo "Error: Package name is required."
+    exit 1
+fi
 
 rm -rf "$PACKAGE"
 uv init --package "$PACKAGE" \
@@ -21,14 +32,10 @@ terraform/.terraform.lock.hcl
 *.zip
 EOF
 
-uvx copier copy "$TEMPLATE_REPO" . \
-  --data "package_name=$PACKAGE" \
-  --data "project_description=$DESCRIPTION" \
-  --overwrite --trust
+uvx copier copy `realpath ../template` . --data "package_name=$PACKAGE" --data "project_description=$DESCRIPTION" --overwrite --trust
 
-uv sync
-uv add "flask[async]" httpx yarl defusedxml python-json-logger aws-wsgi wireup arrow
-uv add ruff pyright pytest pytest-asyncio pytest-cov pytest-docker feedparser pyhamcrest mbtest respx brunns-matchers pyfakefs --dev
+uv add arrow 'httpx[http2]' yarl `gum filter aws-wsgi defusedxml django fastapi flask 'flask[async]' gunicorn pydantic python-json-logger sqlalchemy 'sqlalchemy[asyncio]' wireup --no-limit --header 'Select Packages:'`
+uv add --dev ruff pytest pyright mockito pyhamcrest mbtest pytest-cov pytest-docker `gum filter brunns-matchers freezegun pyfakefs pytest-asyncio pytest-mockito respx scripttest --no-limit --header 'Select Test Packages:'`
 
 xc format
 xc pc
